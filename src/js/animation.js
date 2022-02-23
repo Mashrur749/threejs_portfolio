90682ea6cf54c10789eebad357175a69df6bb513
mashrur749 <mashrur749@gmail.com>
2022-02-22 20:12:07 -0500

    loader done from gsap instead of keyframe

--- src/js/animation.js
+++ src/js/animation.js
@@ -7,15 +7,48 @@ const timeline = gsap.timeline({
 });
 
 timeline
-  .from("nav", {
-    y: -100,
-    opacity: 0,
-    delay: 2,
+  .from(".ripple", {
+    x: -0,
+    y: -120,
   })
-  .from("#headshot", {
-    y: -100,
-    opacity: 0,
-  })
+  .from(
+    ".ripple",
+    {
+      opacity: 0,
+      height: 0,
+      width: 0,
+      opacity: 1,
+      ease: "Sine.easeOut",
+      duration: 3,
+      stagger: 0.15,
+      delay: 0.5,
+    },
+    "<.3"
+  )
+  .to(
+    "#loader",
+    {
+      opacity: 0,
+      visibility: "hidder",
+      duration: 2,
+    },
+    "<"
+  )
+  .from(
+    "nav a",
+    {
+      y: -100,
+      opacity: 0,
+    },
+    "<.5"
+  )
+  .from(
+    "#headshot",
+    {
+      opacity: 0,
+    },
+    "<.8"
+  )
   .from(
     "#site-header",
     {
@@ -24,9 +57,13 @@ timeline
     },
     "<"
   )
-  .from("#arrow-icon", {
-    opacity: 0,
-  })
+  .from(
+    "#arrow-icon",
+    {
+      opacity: 0,
+    },
+    "<.5"
+  )
   .from(
     "#arrow-icon",
     {
